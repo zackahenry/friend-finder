80d53c9e160a3b6575e55f1af8f602fc5c3b2c99
Suntex Marinas <Marketing@Suntexs-MacBook-Pro.local>
2017-12-06 12:36:04 -0600

    commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require("path");
+
+var PORT = process.env.PORT || 8080
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json'}));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
+app.use(bodyParser.text({ type: 'text/html'}));
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: "+ PORT);
+});
